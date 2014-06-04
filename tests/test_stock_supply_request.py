fa9b1dc48a1b03a62d8d39b7b67f0e2e833874a0
Guillem Barba <guillem@nan-tic.com>
2014-06-04 13:05:33 +0200

    Remove __main__ entry point and PATH manipulation in test

--- tests/test_stock_supply_request.py
+++ tests/test_stock_supply_request.py
@@ -1,19 +1,11 @@
 #!/usr/bin/env python
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-
-import sys
-import os
-DIR = os.path.abspath(os.path.normpath(os.path.join(__file__,
-    '..', '..', '..', '..', '..', 'trytond')))
-if os.path.isdir(DIR):
-    sys.path.insert(0, os.path.dirname(DIR))
-
 import unittest
 import trytond.tests.test_tryton
 from decimal import Decimal
-from trytond.tests.test_tryton import POOL, DB_NAME, USER, CONTEXT, test_view,\
-    test_depends
+from trytond.tests.test_tryton import (POOL, DB_NAME, USER, CONTEXT, test_view,
+    test_depends)
 from trytond.transaction import Transaction
 
 
@@ -143,6 +135,3 @@ def suite():
             suite.addTest(test)
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(TestCase))
     return suite
-
-if __name__ == '__main__':
-    unittest.TextTestRunner(verbosity=2).run(suite())
